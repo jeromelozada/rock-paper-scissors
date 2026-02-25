8da19f4b370c15bd10562fc9d60499eda77f2acf
Jerome Lozada <29458966+thisisjer0m3@users.noreply.github.com>
2026-02-25 16:33:36 +0800

    create function to log random choices

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello Jerome");
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3);
+
+  switch (random) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    default:
+      return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
