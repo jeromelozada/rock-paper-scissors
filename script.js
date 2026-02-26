78f6af012a76425eff0e4be8de1525b1d558e35b
Jerome Lozada <29458966+thisisjer0m3@users.noreply.github.com>
2026-02-26 21:47:36 +0800

    Added new function to get the choices from the first 2 functions

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 //This will get whatever the user input
 function getHumanChoice(userChoice) {
@@ -21,8 +21,18 @@ function getHumanChoice(userChoice) {
 
 const userChoice = prompt("Enter your choice here:");
 
-console.log(getHumanChoice(userChoice));
+// console.log(getHumanChoice(userChoice));
 
 //For tracking player's score
 let humanScore = 0;
 let computerScore = 0;
+
+// This function is to get choices from function getHumanChoice and getComputerChoice
+function playRound(humanChoice, computerChoice) {
+  return humanChoice.toLowerCase() + " | " + computerChoice;
+}
+
+const humanSelection = getHumanChoice(userChoice);
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
