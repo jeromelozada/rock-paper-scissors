01034f89bdb12ae96e3f1c3c6dae37ff6b80ecec
Jerome Lozada <29458966+thisisjer0m3@users.noreply.github.com>
2026-03-01 15:52:26 +0800

    updated code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//For tracking player's score
+let humanScore = 0;
+let computerScore = 0;
+
 //This will get computer choice randomly
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
@@ -11,28 +15,39 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-
 // console.log(getComputerChoice());
 
 //This will get whatever the user input
-function getHumanChoice(userChoice) {
+function getHumanChoice() {
+  const userChoice = prompt("Enter your choice here:");
   return userChoice;
 }
 
-const userChoice = prompt("Enter your choice here:");
-
 // console.log(getHumanChoice(userChoice));
-
-//For tracking player's score
-let humanScore = 0;
-let computerScore = 0;
 
 // This function is to get choices from function getHumanChoice and getComputerChoice
 function playRound(humanChoice, computerChoice) {
-  return humanChoice.toLowerCase() + " | " + computerChoice;
-}
+  humanChoice = humanChoice.toLowerCase();
 
-const humanSelection = getHumanChoice(userChoice);
+  let result;
+
+  if (humanChoice === computerChoice) {
+    result = "Draw!";
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log((humanScore += 1));
+    result = `You win! ${humanChoice} beats ${computerChoice}`;
+  } else {
+    console.log((computerScore += 1));
+    result = `You lose! ${computerChoice} beats ${humanChoice}`;
+  }
+
+  return `You: ${humanChoice} | Computer: ${computerChoice} | Result: ${result}`;
+}
+const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 console.log(playRound(humanSelection, computerSelection));
